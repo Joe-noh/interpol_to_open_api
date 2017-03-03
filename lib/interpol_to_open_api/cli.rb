7c6f1f105a4889fb2cf564c122995e601e43ddb4
Joe-noh <honzawa.j@pepabo.com>
2017-03-03 18:50:47 +0900

    convert perameters nad responses

--- lib/interpol_to_open_api/cli.rb
+++ lib/interpol_to_open_api/cli.rb
@@ -1,12 +1,42 @@
+require 'optparse'
+
 module InterpolToOpenAPI
   class CLI
-    def initialize
-      p ARGV
+    def initialize(argv)
+      @glob = nil
+      @output = nil
+
+      define_cli_options(argv)
     end
 
     def run
-      converter = InterpolToOpenAPI::Converter.new
-      p converter
+      converter = InterpolToOpenAPI::Converter.new(
+        src: @glob,
+        dest: @output
+      )
+
+      converter.convert
+    end
+
+    private
+
+    def define_cli_options(argv)
+      @opt = OptionParser.new
+
+      @opt.on('-v', '--version', 'print the version') do
+        puts InterpolToOpenAPI::VERSION
+        exit
+      end
+
+      @opt.on('-i path', '--input', 'path to input endpoint definition yaml') do |path|
+        @glob = path
+      end
+
+      @opt.on('-o path', '--output', 'path to output open api yaml') do |path|
+        @output = path
+      end
+
+      @opt.parse!(argv)
     end
   end
 end
--- lib/interpol_to_open_api/converter.rb
+++ lib/interpol_to_open_api/converter.rb
@@ -1,10 +1,70 @@
+require 'yaml'
+
+class String
+  def camelize
+    self.split("_").map {|w|
+      w[0] = w[0].upcase
+      w
+    }.join
+  end
+end
+
 module InterpolToOpenAPI
   class Converter
-    def initialize
+    def initialize(opts)
+      @files = Dir.glob(opts[:src])
+      @dest = opts[:dest]
     end
 
     def convert
-      p 'hello from convert'
+      interpols = @files.map {|file| YAML.load_file(file) }
+      p (
+        interpols.group_by {|interpol| interpol['method'].downcase }.inject({}) do |acc, (method, routes)|
+          acc[method] = routes.map do |route|
+            req, res = route['definitions'].partition {|definition| definition['message_type'] == 'request' }
+
+            parameters = (req.first['path_params']['properties'] || []).map {|(name, schema)|
+              {
+                in: 'path',
+                name: name.camelize,
+                description: '',
+                required: true,
+                type: schema['type']
+              }
+            }
+            parameters << (req.first['query_params'] || []).map {|name, schema|
+              {
+                in: 'query',
+                name: name.camelize,
+                description: '',
+                type: schema['type']
+              }
+            }
+            parameters << (req.first['schema']['properties'] || []).map {|name, schema|
+              {
+                in: 'body',
+                name: name.camelize,
+                description: '',
+                schema: schema
+              }
+            }
+
+            status_code = res.first['status_codes'].first
+            responses = {}
+            responses[status_code] = {
+              description: '',
+              schema: res.first['schema']
+            }
+
+            {
+              responses: responses,
+              parameters: parameters
+            }
+          end
+
+          acc
+        end
+      )
     end
   end
 end
