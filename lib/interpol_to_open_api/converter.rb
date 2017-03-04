320f622f7e754af28d504d0530084fc477dcf743
Joe-noh <goflb.jh@gmail.com>
2017-03-04 17:22:10 +0900

    convert file one by one

--- lib/interpol_to_open_api/converter.rb
+++ lib/interpol_to_open_api/converter.rb
@@ -11,60 +11,52 @@ end
 
 module InterpolToOpenAPI
   class Converter
-    def initialize(opts)
-      @files = Dir.glob(opts[:src])
-      @dest = opts[:dest]
-    end
+    def convert(path)
+      interpol = YAML.load_file(path)
 
-    def convert
-      interpols = @files.map {|file| YAML.load_file(file) }
-      p (
-        interpols.group_by {|interpol| interpol['method'].downcase }.inject({}) do |acc, (method, routes)|
-          acc[method] = routes.map do |route|
-            req, res = route['definitions'].partition {|definition| definition['message_type'] == 'request' }
+      req, res = interpol['definitions'].partition {|definition| definition['message_type'] == 'request' }
 
-            parameters = (req.first['path_params']['properties'] || []).map {|(name, schema)|
-              {
-                in: 'path',
-                name: name.camelize,
-                description: '',
-                required: true,
-                type: schema['type']
-              }
-            }
-            parameters << (req.first['query_params'] || []).map {|name, schema|
-              {
-                in: 'query',
-                name: name.camelize,
-                description: '',
-                type: schema['type']
-              }
-            }
-            parameters << (req.first['schema']['properties'] || []).map {|name, schema|
-              {
-                in: 'body',
-                name: name.camelize,
-                description: '',
-                schema: schema
-              }
-            }
+      parameters = (req.first['path_params']['properties'] || []).map {|(name, schema)|
+        {
+          'in' => 'path',
+          'name' => name.camelize,
+          'description' => '',
+          'required' => true,
+          'type' => schema['type']
+        }
+      }
+      parameters << (req.first['query_params'] || []).map {|name, schema|
+        {
+          'in' => 'query',
+          'name' => name.camelize,
+          'description' => '',
+          'type' => schema['type']
+        }
+      }
+      parameters << (req.first['schema']['properties'] || []).map {|name, schema|
+        {
+          'in' => 'body',
+          'name' => name.camelize,
+          'description' => '',
+          'schema' => schema
+        }
+      }
 
-            status_code = res.first['status_codes'].first
-            responses = {}
-            responses[status_code] = {
-              description: '',
-              schema: res.first['schema']
-            }
+      status_code = res.first['status_codes'].first
+      responses = {}
+      responses[status_code] = {
+        'description' => '',
+        'schema' => res.first['schema']
+      }
 
-            {
-              responses: responses,
-              parameters: parameters
-            }
-          end
-
-          acc
-        end
-      )
+      {
+        interpol['route'] => {
+          interpol['method'].downcase => {
+            'responses' => responses,
+            'parameters' => parameters
+          }
+        }
+      }
     end
   end
 end
--- test/interpol_to_open_api/converter_test.rb
+++ test/interpol_to_open_api/converter_test.rb
@@ -2,9 +2,10 @@ require 'test_helper'
 
 class InterpolToOpenAPI::ConverterTest < Minitest::Test
   def test_conversion
-    converter = InterpolToOpenAPI::Converter.new(src: File.expand_path('../../fixtures/sample.interpol.yaml', __FILE__))
+    converter = InterpolToOpenAPI::Converter.new
+    path = File.expand_path('../../fixtures/sample.interpol.yaml', __FILE__)
     expected = YAML.load_file(File.expand_path('../../fixtures/sample.openapi.yaml', __FILE__))
 
-    assert_equal converter.convert, expected
+    assert_equal converter.convert(path), expected
   end
 end
