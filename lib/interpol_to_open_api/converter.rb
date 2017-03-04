1c2764c0c90802ee77abab56c733e66fb9ed2a1c
Joe-noh <goflb.jh@gmail.com>
2017-03-04 18:24:46 +0900

    mostly match

--- lib/interpol_to_open_api/converter.rb
+++ lib/interpol_to_open_api/converter.rb
@@ -16,7 +16,38 @@ module InterpolToOpenAPI
 
       req, res = interpol['definitions'].partition {|definition| definition['message_type'] == 'request' }
 
-      parameters = (req.first['path_params']['properties'] || []).map {|(name, schema)|
+      parameters = []
+      parameters += parameters_in_path(req.first['path_params']['properties'])
+      parameters += parameters_in_query(req.first['query_params']['properties'])
+      parameters += parameters_in_body(req.first['schema'])
+
+      status_code = res.first['status_codes'].first
+      responses = {}
+      responses[status_code] = {
+        'description' => '',
+        'schema' => res.first['schema'].merge({
+          'example' => res.first['examples'].first
+        })
+      }
+
+      {
+        interpol['route'] => {
+          interpol['method'].downcase => {
+            'summary' => '',
+            'description' => req.first['schema']['description'] || '',
+            'parameters' => parameters,
+            'responses' => responses
+          }
+        }
+      }
+    end
+
+    private
+
+    def parameters_in_path(properties)
+      return [] unless properties.is_a? Hash
+
+      properties.map do |name, schema|
         {
           'in' => 'path',
           'name' => name.camelize,
@@ -24,39 +55,33 @@ module InterpolToOpenAPI
           'required' => true,
           'type' => schema['type']
         }
-      }
-      parameters << (req.first['query_params'] || []).map {|name, schema|
+      end
+    end
+
+    def parameters_in_query(properties)
+      return [] unless properties.is_a? Hash
+
+      properties.map do |name, schema|
         {
           'in' => 'query',
-          'name' => name.camelize,
+          'name' => name,
           'description' => '',
           'type' => schema['type']
         }
-      }
-      parameters << (req.first['schema']['properties'] || []).map {|name, schema|
+      end
+    end
+
+    def parameters_in_body(schema)
+      return [] unless schema.is_a? Hash
+
+      schema['properties'].map do |name, schema|
         {
           'in' => 'body',
-          'name' => name.camelize,
+          'name' => name,
           'description' => '',
           'schema' => schema
         }
-      }
-
-      status_code = res.first['status_codes'].first
-      responses = {}
-      responses[status_code] = {
-        'description' => '',
-        'schema' => res.first['schema']
-      }
-
-      {
-        interpol['route'] => {
-          interpol['method'].downcase => {
-            'responses' => responses,
-            'parameters' => parameters
-          }
-        }
-      }
+      end
     end
   end
 end
