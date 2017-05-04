0ad84020fd4065865289f30c83effb61dfd0264b
Joe-noh <goflb.jh@gmail.com>
2017-05-04 17:57:16 +0900

    property class knows how to convert properties into openapi

--- lib/interpol_to_open_api/interpol/schema.rb
+++ lib/interpol_to_open_api/interpol/schema.rb
@@ -1,3 +1,5 @@
+require_relative "property"
+
 module InterpolToOpenAPI
   module Interpol
     class Schema
@@ -6,57 +8,10 @@ module InterpolToOpenAPI
       end
 
       def to_openapi
-        converted = @schema.map do |key, val|
-          case key
-          when "properties"
-            [key, x_nullable(val)]
-          else
-            [key, val]
-          end
-        end
+        update = {}
+        update["properties"] = Property.array_to_openapi(@schema["properties"]) if @schema.has_key?("properties")
 
-        Hash[converted]
-      end
-
-      private
-
-      def x_nullable(properties)
-        converted = properties.map do |key, val|
-          if val.is_a?(Hash)
-            if val["type"].is_a?(Array)
-              [key, extract_nullable(val)]
-            elsif val.has_key?("properties")
-              [key, Schema.new(val).to_openapi]
-            else
-              [key, val]
-            end
-          else
-            [key, val]
-          end
-        end
-
-        Hash[converted]
-      end
-
-      def extract_nullable(prop)
-        if prop["type"].member? "null"
-          prop.merge({
-            "type" => type_without_null(prop["type"]),
-            "x-nullable" => true
-          })
-        else
-          prop
-        end
-      end
-
-      def type_without_null(type)
-        new_type = type.select {|t| t != "null"}
-
-        if new_type.size == 1
-          new_type.first
-        else
-          new_type
-        end
+        @schema.merge(update)
       end
     end
   end
